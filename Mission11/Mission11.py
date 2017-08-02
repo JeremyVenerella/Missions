3958118f0fbf2b6c1dfc17f44fddd34f6765fca5
Jeremy Venerella <jer20084@gmail.com>
2017-08-02 17:33:34 -0500

    Finished

---
+++ Mission11/Mission11.py
@@ -0,0 +1,17 @@
+def check_password(s):
+    good = "4e5d4e92865a4e495a86494b5a5d49525261865f5758534d4a89".decode("hex")
+
+    if (len(s) != len(good)):
+        return False
+
+    return all(((ord(cs)-89)&255^115^50 == ord(cg)) for cs, cg in zip(s, good))
+
+def decode_password():
+    decode = ""
+    good = "4e5d4e92865a4e495a86494b5a5d49525261865f5758534d4a89".decode("hex")   
+    for cs in good:
+        decode += chr((89+(ord(cs)^50^115)&255))
+    return decode
+
+print(decode_password())
+print(check_password(decode_password()))
